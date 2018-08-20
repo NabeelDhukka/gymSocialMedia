5b14a93fcdb2d4f83b1de92a0e3765483c418fe9
NabeelDhukka <Nabeeldhukka@gmail.com>
2018-08-20 14:25:14 -0500

    cardio fragment layout

--- app/src/main/java/com/example/nabee/gymsocialmedia/excerciseLogging/CardioFragment.java
+++ app/src/main/java/com/example/nabee/gymsocialmedia/excerciseLogging/CardioFragment.java
@@ -6,27 +6,34 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.nabee.gymsocialmedia.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CardioFragment extends Fragment {
     private static final String TAG = "CardioFragment";
-
-    //private Button btnTEST;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_cardio,container,false);
-        //btnTEST = (Button) view.findViewById(R.id.btnTEST);
-
-        /*btnTEST.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getActivity(), "TESTING BUTTON CLICK 1",Toast.LENGTH_SHORT).show();
-            }
-        });*/
 
         return view;
     }
+
+//    public void fillSpinner(){
+//        //list to fill spinner
+//        ArrayList<String> exers = new ArrayList<>();
+//        exers.add("new exer");
+//        ArrayAdapter<String> exerListAdapter = new ArrayAdapter<String>(getContext(),android.R.layout.simple_spinner_item ,exers);
+//        exerListAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        //Spinner elst = null;
+//        Spinner elst = (Spinner)findViewById(R.id.cardioList);
+//        elst.setAdapter(exerListAdapter);
+//        findVire
+//    }
 }
